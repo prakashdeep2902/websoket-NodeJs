94072f7436a651c9e15f1779bea494a30d7e0908
sharmaJi <prakashdeep2902@gmail.com>
2025-02-02 16:58:01 +0530

    added something in public

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`a new user has connected ${socket.id}`);
+});
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
